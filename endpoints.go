af6235a0e557946124e9e8225b956dd1f61c975a
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-19 16:17:51 +0200

    adds endpoints

---
+++ endpoints.go
@@ -0,0 +1,16 @@
+package dhelpers
+
+import "github.com/bwmarrin/discordgo"
+
+// sets endpoints with a custom base discord host
+// https://github.com/bwmarrin/discordgo/blob/master/endpoints.go#L181
+func SetDiscordEndpoints(endpointDiscord string) {
+	discordgo.EndpointDiscord = endpointDiscord
+	discordgo.EndpointAPI = discordgo.EndpointDiscord + "api/v" + discordgo.APIVersion + "/"
+	discordgo.EndpointGuilds = discordgo.EndpointAPI + "guilds/"
+	discordgo.EndpointChannels = discordgo.EndpointAPI + "channels/"
+	discordgo.EndpointUsers = discordgo.EndpointAPI + "users/"
+	discordgo.EndpointGateway = discordgo.EndpointAPI + "gateway"
+	discordgo.EndpointGatewayBot = discordgo.EndpointGateway + "/bot"
+	discordgo.EndpointWebhooks = discordgo.EndpointAPI + "webhooks/"
+}
