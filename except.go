3a0344fa3ca9b78ee627e2c2b4029ad4d8430f95
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-04-22 13:32:38 +0200

    don't log discord permission errors
    Signed-off-by: Sebastian Winkler <sebastian.winkler@slmn.de>

--- except.go
+++ except.go
@@ -5,6 +5,9 @@ import (
 
 	"fmt"
 
+	"math/rand"
+	"time"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/getsentry/raven-go"
@@ -31,9 +34,23 @@ func HandleErrWith(service string, err error, errorHandlers []ErrorHandlerType, 
 		msg = event.MessageUpdate.Message
 	}
 
+	var dontLog bool
+	// don't log discord permission errors
+	if errD, ok := err.(*discordgo.RESTError); ok {
+		if errD.Message.Code == discordgo.ErrCodeMissingPermissions ||
+			errD.Message.Code == discordgo.ErrCodeMissingAccess ||
+			errD.Message.Code == discordgo.ErrCodeCannotSendMessagesToThisUser {
+			dontLog = true
+		}
+	}
+
 	for _, errorHandlerType := range errorHandlers {
 		switch errorHandlerType {
 		case SentryErrorHandler:
+			if dontLog {
+				continue
+			}
+
 			if raven.ProjectID() != "" {
 				// send error to sentry
 				data := map[string]string{"service": service}
@@ -50,6 +67,7 @@ func HandleErrWith(service string, err error, errorHandlers []ErrorHandlerType, 
 		case DiscordErrorHandler:
 			if msg != nil {
 				// send message to discord
+				// TODO: check channel permissions, if no permission to send message add reaction using addPermissionsErrorReaction
 				SendMessage( // nolint: errcheck
 					msg.ChannelID,
 					"**Something went wrong.** <a:ablobsadcloud:437572939701944322>\n```\nError: "+err.Error()+"\n```I sent our top people to fix the issue as soon as possible. <a:ablobreach:437572330026434560>",
@@ -58,9 +76,25 @@ func HandleErrWith(service string, err error, errorHandlers []ErrorHandlerType, 
 		}
 	}
 
-	// log stacktrace
-	buf := make([]byte, 1<<16)
-	stackSize := runtime.Stack(buf, false)
+	if !dontLog {
+		// log stacktrace
+		buf := make([]byte, 1<<16)
+		stackSize := runtime.Stack(buf, false)
 
-	cache.GetLogger().Errorln(err.Error() + "\n\n" + string(buf[0:stackSize]))
+		cache.GetLogger().Errorln(err.Error() + "\n\n" + string(buf[0:stackSize]))
+	}
+}
+
+func addPermissionsErrorReaction(channelID, messageID string) {
+	reactions := []string{
+		":blobstop:317034621953114112",
+		"a:ablobweary:394026914479865856",
+		":googlespeaknoevil:317036753074651139",
+		":notlikeblob:349342777978519562",
+		"a:ablobcry:393869333740126219",
+		"a:ablobfrown:394026913292615701",
+		"a:ablobunamused:393869335573037057",
+	}
+	rand.Seed(time.Now().Unix())
+	cache.GetDiscord().MessageReactionAdd(channelID, messageID, reactions[rand.Intn(len(reactions))]) // nolint: errcheck
 }
