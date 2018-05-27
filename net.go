264d97cdad1671465ccefc0c9b22de32c01b6190
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-05-27 14:05:38 +0200

    [net] adds gzip reading
    Signed-off-by: Sebastian Winkler <sebastian.winkler@slmn.de>

--- net.go
+++ net.go
@@ -2,6 +2,7 @@ package dhelpers
 
 import (
 	"bytes"
+	"compress/gzip"
 	"errors"
 	"io"
 	"net/http"
@@ -58,34 +59,60 @@ func NetGetTimeout(url string, timeout time.Duration) ([]byte, error) {
 	// Prepare request
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
+	// set user agent header
 	request.Header.Set("User-Agent", defaultUA)
+	// allow receiving gzip
+	request.Header.Add("Accept-Encoding", "gzip")
 
 	// Do request
 	response, err := GetHTTPClientTimeoutWithoutKeepAlive(timeout).Do(request)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 
 	// Only continue if code was 200 - 299
 	if response.StatusCode < 200 || response.StatusCode > 299 {
 		if err != nil {
-			return []byte{}, errors.New("expected status 200; got " + strconv.Itoa(response.StatusCode))
+			return nil, errors.New("expected status 200; got " + strconv.Itoa(response.StatusCode))
 		}
-	} else {
-		// Read body
-		if response.Body != nil {
-			defer response.Body.Close() // nolint: errcheck
-		}
+	}
 
-		buf := bytes.NewBuffer(nil)
+	// Read body
+	if response.Body != nil {
+		defer response.Body.Close() // nolint: errcheck
+	}
+
+	// create bytes buffer
+	buf := bytes.NewBuffer(nil)
+
+	// read content-encoding
+	switch response.Header.Get("Content-Encoding") {
+	case "gzip":
+		// decompress gzip if required
+		var gzipReader *gzip.Reader
+		gzipReader, err = gzip.NewReader(response.Body)
+		if err != nil {
+			return nil, err
+		}
+		// close reader
+		if gzipReader != nil {
+			defer gzipReader.Close() // nolint: errcheck
+		}
+		// copy result to buffer
+		_, err = io.Copy(buf, gzipReader)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		// copy raw data to buffer
 		_, err := io.Copy(buf, response.Body)
 		if err != nil {
-			return []byte{}, err
+			return nil, err
 		}
-
-		return buf.Bytes(), nil
 	}
-	return []byte{}, errors.New("internal error")
+
+	// return bytes
+	return buf.Bytes(), nil
 }
