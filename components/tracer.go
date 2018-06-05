f18d86e0922a6faecdade86b14ebecdb3ecd6070
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-06-05 13:04:02 +0200

    updates DataDog tracing module
    Signed-off-by: Sebastian Winkler <sebastian.winkler@slmn.de>

--- components/tracer.go
+++ components/tracer.go
@@ -1,39 +1,19 @@
 package components
 
 import (
-	"io"
-
-	ddtrace "github.com/DataDog/dd-trace-go/opentracing"
 	"github.com/opentracing/opentracing-go"
 	"gitlab.com/Cacophony/dhelpers/cache"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/opentracer"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
-
-var tracerCloser io.Closer
 
 // InitTracer initialises the opentracing tracer and datadog integration
 func InitTracer(service string) error {
-	var err error
-	var tracer opentracing.Tracer
-
-	config := ddtrace.NewConfiguration()
-	config.ServiceName = service
-
-	tracer, tracerCloser, err = ddtrace.NewTracer(config)
-	if err != nil {
-		return err
-	}
+	t := opentracer.New(tracer.WithServiceName(service))
 
 	// set the Datadog tracer as a cached tracer
-	cache.SetTracer(tracer)
+	cache.SetTracer(t)
 	// set the Datadog tracer as a GlobalTracer
-	opentracing.SetGlobalTracer(tracer)
-	return nil
-}
-
-// UninitTracer uninitialises the opentracing tracer and datadog integration
-func UninitTracer() error {
-	if tracerCloser != nil {
-		return tracerCloser.Close()
-	}
+	opentracing.SetGlobalTracer(t)
 	return nil
 }
