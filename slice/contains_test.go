6b4c90b9a4d40f783cf80379b21e22bde18e9aed
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-06-05 13:04:59 +0200

    lints
    Signed-off-by: Sebastian Winkler <sebastian.winkler@slmn.de>

--- slice/contains_test.go
+++ slice/contains_test.go
@@ -4,21 +4,21 @@ import "testing"
 
 func TestContainsLowerExclude(t *testing.T) {
 	vIncludes, vNewArgs := ContainsLowerExclude([]string{"aaa", "bbb", "ccc"}, nil)
-	if vIncludes == true {
+	if vIncludes {
 		t.Error("Excepcted includes to be false, received ", vIncludes)
 	}
 	if len(vNewArgs) != 3 {
 		t.Error("Expected 3 newArgs, received ", len(vNewArgs))
 	}
 	vIncludes, vNewArgs = ContainsLowerExclude([]string{"aaa", "bbb", "ccc"}, []string{"aaa"})
-	if vIncludes == false {
+	if !vIncludes {
 		t.Error("Excepcted includes to be true, received ", vIncludes)
 	}
 	if len(vNewArgs) != 2 {
 		t.Error("Expected 2 newArgs, received ", len(vNewArgs))
 	}
 	vIncludes, vNewArgs = ContainsLowerExclude([]string{"aaa", "bbb", "ccc"}, []string{"eee"})
-	if vIncludes == true {
+	if vIncludes {
 		t.Error("Excepcted includes to be false, received ", vIncludes)
 	}
 	if len(vNewArgs) != 3 {
