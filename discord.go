c53c13f0631df8437e8cb330967e3d78100fadcc
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-08-16 22:25:40 +0200

    makes channelID parameter for event.GoType optional
    Signed-off-by: Sebastian Winkler <sebastian.winkler@slmn.de>

--- discord.go
+++ discord.go
@@ -9,13 +9,50 @@ import (
 )
 
 // GoType starts a goroutine to start a typing indicator in a channel
-func (event EventContainer) GoType(channelID string) {
+// it will use the channelID if given, if not it will try to use the channelID from the event,
+// the following events are supported at the moment:
+// MESSAGE_CREATE, MESSAGE_UPDATE, MESSAGE_DELETE, MESSAGE_REACTION_ADD, MESSAGE_REACTION_REMOVE,
+//  MESSAGE_REACTION_REMOVE_ALL, CHANNEL_CREATE, CHANNEL_UPDATE, CHANNEL_DELETE, CHANNEL_PINS_UPDATE
+func (event EventContainer) GoType(channelID ...string) {
 	go func() {
 		defer func() {
 			recover() // nolint: errcheck, gas
 		}()
 
-		cache.GetEDiscord(event.BotUserID).ChannelTyping(channelID) // nolint: errcheck, gas
+		var typingChannel string
+
+		if len(channelID) > 0 {
+			// use channel ID if given
+			typingChannel = channelID[0]
+		} else {
+			// or try to use channel from event
+			switch event.Type {
+			case MessageCreateEventType:
+				typingChannel = event.MessageCreate.ChannelID
+			case MessageUpdateEventType:
+				typingChannel = event.MessageUpdate.ChannelID
+			case MessageDeleteEventType:
+				typingChannel = event.MessageDelete.ChannelID
+			case MessageReactionAddEventType:
+				typingChannel = event.MessageReactionAdd.ChannelID
+			case MessageReactionRemoveEventType:
+				typingChannel = event.MessageReactionRemove.ChannelID
+			case MessageReactionRemoveAllEventType:
+				typingChannel = event.MessageReactionRemoveAll.ChannelID
+			case ChannelCreateEventType:
+				typingChannel = event.ChannelCreate.ID
+			case ChannelUpdateEventType:
+				typingChannel = event.ChannelUpdate.ID
+			case ChannelDeleteEventType:
+				typingChannel = event.ChannelDelete.ID
+			case ChannelPinsUpdateEventType:
+				typingChannel = event.ChannelPinsUpdate.ChannelID
+			}
+		}
+
+		if typingChannel != "" {
+			cache.GetEDiscord(event.BotUserID).ChannelTyping(typingChannel) // nolint: errcheck, gas
+		}
 	}()
 }
 
